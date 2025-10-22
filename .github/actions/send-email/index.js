c5bd34f3d8787caa738f1dbef4c5c6095565a6d0
Jordi Valls Pla <jordivallspladaw@gmail.com>
2025-10-22 18:56:38 +0200

    error 3

--- .github/actions/send-email/index.js
+++ .github/actions/send-email/index.js
@@ -1,5 +1,8 @@
 const nodeMailer = require('nodemailer');  
 
+console.log(process.env.USUARIO_EMAIL);
+console.log(process.env.EMAIL);
+
 const enviador =  nodeMailer.createTransport({
     service: "gmail",
     auth: {
@@ -9,13 +12,18 @@ const enviador =  nodeMailer.createTransport({
     }
 });
 
-const enviar = async () => {
+
+async function enviar() {
     await enviador.sendMail({
         from: process.env.USUARIO_EMAIL,
         to: "jordivallspladaw@gmail.com",
         subject: "Resultat del workflow executat",
         html: "<p>S'ha realitzat un push en la branca main que ha provocat l'execució del workflow nom_repositori_workflow amb els següents resultats:</p>",
     });
-};
+}
 
-enviar();
+enviar().then(() => {   
+    console.log("Email enviat correctament");
+}).catch((error) => {
+    console.error("Error en enviar l'email:", error);
+});
